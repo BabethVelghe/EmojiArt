796f440449560b12809c4d239e79f2b7366e4aa4
Babeth Velghe <babeth.velghe@student.hogent.be>
2023-10-17 15:43:32 +0200

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Babeth Velghe on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
